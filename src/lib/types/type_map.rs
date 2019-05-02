4f71def76db17899dc4be9390be6e29366233deb
g2_xpf <nameko.jack@gmail.com>
2019-05-02 22:47:27 +0900

    wrapped TypeMap for debug messages

--- src/lib/types/type_map.rs
+++ src/lib/types/type_map.rs
@@ -2,4 +2,32 @@ use super::ComponentMask;
 use std::any::TypeId;
 use std::collections::HashMap;
 
-pub type TypeMap = HashMap<TypeId, ComponentMask>;
+pub struct TypeMap(HashMap<TypeId, ComponentMask>);
+
+impl TypeMap {
+    #[inline]
+    pub fn new() -> Self {
+        TypeMap(HashMap::new())
+    }
+
+    #[inline]
+    pub fn get(&self, type_id: &TypeId) -> &ComponentMask {
+        self.0
+            .get(type_id)
+            .unwrap_or_else(|| panic!("Type dispatch failed"))
+    }
+
+    #[inline]
+    pub fn get_mut(&mut self, type_id: &TypeId) -> &mut ComponentMask {
+        self.0
+            .get_mut(type_id)
+            .unwrap_or_else(|| panic!("Type dispatch failed"))
+    }
+
+    #[inline]
+    pub fn insert(&mut self, type_id: TypeId, component_mask: ComponentMask) {
+        if let Some(_) = self.0.insert(type_id, component_mask) {
+            panic!("The type is already registered");
+        }
+    }
+}
